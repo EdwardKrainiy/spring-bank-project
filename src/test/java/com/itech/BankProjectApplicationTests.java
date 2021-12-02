39a254c109790885479ecc04144d2be2d355a47b
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-02 12:29:03 +0300

    Initial project created.

---
+++ src/test/java/com/itech/BankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
