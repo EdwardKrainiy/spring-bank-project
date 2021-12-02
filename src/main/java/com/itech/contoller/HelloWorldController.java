feeb863080d3301f57e62a2079306b6d3d202f59
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-02 13:52:42 +0300

    Swagger configured sucessfully.

--- src/main/java/com/itech/contoller/HelloWorldController.java
+++ src/main/java/com/itech/contoller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.itech.contoller;
 
+import io.swagger.annotations.Api;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HelloWorldController {
 
     @GetMapping("/helloworld")
