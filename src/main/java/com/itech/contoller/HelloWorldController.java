c5d41969e923bbda1914d236e6ae49fb57c250a5
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-02 13:11:47 +0300

    Added mapstruct dependency.

--- src/main/java/com/itech/contoller/HelloWorldController.java
+++ src/main/java/com/itech/contoller/HelloWorldController.java
@@ -1,9 +1,7 @@
 package com.itech.contoller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
