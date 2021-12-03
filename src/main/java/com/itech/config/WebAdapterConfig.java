23bc54f7ecc34584b4643d9f5eab542a04692221
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-03 13:33:51 +0300

    Added some necessary files.

---
+++ src/main/java/com/itech/config/WebAdapterConfig.java
@@ -0,0 +1,49 @@
+package com.itech.config;
+
+import com.itech.model.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebAdapterConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/swagger-ui.html").hasAnyRole(Role.MANAGER.name(), Role.USER.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("admin")
+                        .password(encoder().encode("admin"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+
+    @Bean
+    protected PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+}
