4e05af80e0d642ee181a0df42bec91b262cd3c0a
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-03 12:53:21 +0300

    Minor changes on pom.xml. Added some dependencies.

--- src/main/java/com/itech/config/SwaggerConfig.java
+++ src/main/java/com/itech/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.itech.configuration;
+package com.itech.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
---
+++ src/main/java/com/itech/model/Role.java
@@ -0,0 +1,5 @@
+package com.itech.model;
+
+public enum Role {
+    USER, MANAGER
+}
