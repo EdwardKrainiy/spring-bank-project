b858a46ffc595b99972b80d8602364221e70b414
Edvard Krainiy <ekrayniy@inbox.ru>
2021-12-04 17:29:09 +0300

    Removed useless ComponentScan annotation.

--- src/main/java/com/itech/BankProjectApplication.java
+++ src/main/java/com/itech/BankProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.itech.contoller")
 public class BankProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankProjectApplication.class, args);
